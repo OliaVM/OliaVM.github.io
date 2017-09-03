89a245ebc36bb56027320ac5dc731503e068f435
OliaVM <oliamo2016@gmail.com>
2017-09-03 20:40:40 +0700

    2

---
+++ index.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+	<head>
+	  <meta charset="UTF-8">
+	  <link href="/style/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	  <link href="/style/style.css" rel="stylesheet" type="text/css"/>
+	  <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
+	  <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	  <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+	  <link type="text/css" rel="stylesheet" href="/style/bootstrap-responsive.css">
+	</head>
+	<body>
+	
+	<div class="container-fluid">
+	  <div class="row-fluid">
+	    <div class="span2" id="box1"> 
+			<h1>Projects</h1>
+		</div>
+
+		<div class="span10" id="box2">
+			<div>
+				<a href="https://github.com/OliaVM/finalProject/tree/project">Лента новостей</a>
+				<a href="https://github.com/OliaVM/messenger">Messenger</a>
+				<a href="https://github.com/OliaVM/tasks">Викторина</a>
+			</div>
+		</div>
+	  </div>
+  	</div>
+
+	<div class="row-fluid">
+		<div class="span12" id="box3">
+			<h1>2017</h1>>
+		</div>
+	</div>
+	</body>
+</html>
